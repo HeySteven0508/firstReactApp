0cec0cb9178a27c3635fe73b314287d075b5a4e0
HeySteven0508 <concentratify@gmail.com>
2021-03-31 03:16:16 +0800

    Added a json server rest api

--- src/App.js
+++ src/App.js
@@ -8,36 +8,7 @@ import Footer from './components/Footer';
 
 function App() {
   const [showAddButton, setShowAddButton] = useState(false);
-  const [students, setStudents] = useState([
-    {
-      id: 1,
-      name: 'Steven Tan',
-      course: 'BS in Computer Science',
-      age: 26,
-      isScholar: true,
-    },
-    {
-      id: 2,
-      name: 'Mae Princess Abad',
-      course: 'BS in Tourism',
-      age: 27,
-      isScholar: false,
-    },
-    {
-      id: 3,
-      name: 'Celestine Faith',
-      course: 'AB in Communications',
-      age: 20,
-      isScholar: false,
-    },
-    {
-      id: 4,
-      name: 'Charyl Tan',
-      course: 'BS in Commerce',
-      age: 25,
-      isScholar: true,
-    },
-  ]);
+  const [students, setStudents] = useState([]);
 
   //Delete a Student
   const DeleteStudent = (id) => {
